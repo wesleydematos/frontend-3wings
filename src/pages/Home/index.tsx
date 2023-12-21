303bc4d631ddb5e511b45ad21fb8d88c322d5741
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-21 10:37:08 -0300

    feat: update home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,18 @@
+import logo from '../../assets/3wings-logo.webp'
+import { Link } from 'react-router-dom'
+import { FaLongArrowAltRight } from 'react-icons/fa'
+
 function Home() {
   return (
-    <div>Home</div>
+    <div className='flex flex-col justify-center items-center min-h-screen w-screen bg-tx'>
+      <img src={logo} alt='3wings logo' />
+      <Link 
+        to='/products' 
+        className='flex items-center gap-2 text-sm text-white mt-14 border border-white p-4 rounded-2xl hover:border-tertiary-light hover:text-tertiary-light'
+      >
+        Ir para página de produtos <FaLongArrowAltRight className='mt-1'/>
+      </Link>
+    </div>
   )
 }
 
