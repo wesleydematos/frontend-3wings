c76878304a4653fee52c5a876a5e1d2e8802199e
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-21 10:50:01 -0300

    feat: updated products page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,7 +1,24 @@
+import { Link } from 'react-router-dom'
+import { FaHome } from 'react-icons/fa'
+import { useState } from 'react'
+
 function Products() {
+  const [isCreate, setIsCreate] = useState(true)
+
   return (
-    <div>
-      products
+    <div className='flex flex-col min-h-screen w-screen bg-tx text-white'>
+      <header className='flex w-full'>
+        <Link to='/'>
+          <FaHome  className='h-[30px] w-[30px] mt-2 ml-2'/>
+        </Link>
+        <nav className='flex mx-auto gap-5 mt-2 '>
+          <button onClick={()=> setIsCreate(true)}>Criar Produto</button>
+          <button onClick={()=> setIsCreate(false)}>Todos Produtos</button>
+        </nav>
+      </header>
+      {
+        isCreate ? <>create</> : <>all</>
+      }
     </div>
   )
 }
