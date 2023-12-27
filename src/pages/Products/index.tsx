14c10ab5905449b88162aebe3eb2eda03436a60c
Wesley de Matos Souza <104766684+wesleydematos@users.noreply.github.com>
2023-12-27 09:03:26 -0300

    styles: update spacing in header

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -9,8 +9,8 @@ function Products() {
 
   return (
     <div className='flex flex-col min-h-screen w-screen bg-tx text-white'>
-      <header className='flex w-full'>
-        <Link to='/'>
+      <header className='flex w-full mt-1 mb-5 items-center'>
+        <Link to='/' className='ml-5'>
           <FaHome  className='h-[30px] w-[30px] mt-2 ml-2'/>
         </Link>
         <nav className='flex mx-auto gap-5 mt-2 '>
